0a67c9fdf450c7175d66b3f790a07ea61c89b0d1
David <david.cantero808@gmail.com>
2023-07-19 06:11:27 -0400

    nosepudo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Card from '../src/componentes/Card'
-import Card2 from '../src/componentes/Card2'
 
 function App(){
   return (
     <div className='App'>
       <Card/>
-      <Card2/>
     </div>
   )
 }
