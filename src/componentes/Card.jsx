a76b883b7f3c6f97d6b67450859e6482239b5a36
David <david.cantero808@gmail.com>
2023-07-19 15:41:51 -0400

    terminado

--- src/componentes/Card.jsx
+++ src/componentes/Card.jsx
@@ -1,39 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import '../css/estilos.css'
+import { useState, useEffect } from 'react';
+import '../css/estilos.css';
 
-function Card(props){
+function Card(props) {
+  const [pokemon, setPokemon] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-const [pokemon,setPokemon]=useState({});
-const [isLoading,setIsLoading]=useState(true);
-    const url='https://pokeapi.co/api/v2/pokemon/493/'
-    const fethApi=async()=>{
-        const response=await fetch(url)
-        const json=await response.json()
-        setPokemon(json)
+  const fetchData = async () => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+    const data = await response.json();
+    const randomPokemonIndex = Math.floor(Math.random() * data.count) + 1;
+    const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonIndex}`);
+    const pokemonData = await pokemonResponse.json();
+    setPokemon(pokemonData);
+  };
 
-    }
+  useEffect(() => {
+    (async () => {
+      await fetchData();
+      setIsLoading(false);
+    })();
+  }, []);
 
-    useEffect(()=>{
-        (async()=>{
-            await fethApi()
-            setIsLoading(false)
-        })()
-        
-    },[] );
+  if (isLoading) {
+    return <div>Cargando.....</div>;
+  }
 
-if(isLoading){
-    return(
-        <div>Cargando.....</div>
-    )
+  return (
+    <div className='contenedorTexto'>
+      <img className='contenedorImagen' src={pokemon.sprites.front_default} alt='imagen' />
+      <h5 className='contenedorTitulo'>{pokemon.name}</h5>
+      <p className='contenedorParrafo'>Origen:"silvestre"</p>
+    </div>
+  );
 }
 
-return(
-<div className='contenedorTexto'>
-    <img className='contenedorImagen' src={pokemon.sprites.front_default} alt='imagen'/> 
-    <h5 className='contenedorTitulo'> Nombre</h5>
-    <p className='contenedorParrafo'>tipo</p>
-</div>
-
-);
+function CardContainer() {
+  const cards = [];
+  for (let i = 0; i < 10; i++) {
+    cards.push(<Card key={i} />);
+  }
+  return <div className="card-container">{cards}</div>;
 }
-export default Card;
+
+export default CardContainer;
